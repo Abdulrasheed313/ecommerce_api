a7375a34a4e4f18b0b8466a822eaaa4411c51286
Abdul Rasheed313 <64446184+Abdulrasheed313@users.noreply.github.com>
2025-07-02 03:13:27 +0500

    new

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 let getMian = document.querySelector("#main");
 
-
-fetch("https://api.escuelajs.co/api/v1/products")
+fetch('https://fakestoreapi.com/products')
+  .then((data) => data.json())
   .then((data) => {
-    return data.json();
+    console.log(data);
+    data.map((item) => {
+      getMian.innerHTML += `
+        <div class="col-md-4 mb-4">
+          <div class="card text-center  p-4" >
+            <img src="${item.image}" class="card-img-top" alt="${item.title}">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${item.description.substring(0, 50)}...</p>
+              <p class="card-text"><b>Price:</b> $${item.price}</p>
+              <a href="#" class="main_btn">Buy Now</a>
+            </div>
+          </div>
+        </div>
+      `;
+    });
   })
-  .then((data) => {
-    console.log(data)
-    data.map((item)=>{
-        getMian.innerHTML += `<div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-  <img src="${item.images[0]}" class="card-img-top" alt="...">
-  <div class="card-body">
-   <h5 class="card-title">${item.id}</h5>
-    <h5 class="card-title">${item.title}</h5>
-    <p class="card-text">${item.description}</p>
-    <p class="card-text"><b>Price:</b> ${item.price}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        
-        
-        </div>`
-    })
-  })
-  .catch((err) => {
-    console.error("Fetch error:", err);
+  .catch((error) => {
+    console.error("Error fetching products:", error);
   });
